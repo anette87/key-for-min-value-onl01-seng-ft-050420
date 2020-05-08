be9fdbc2469086b279d797c2e50569ee4d307f5e
Anette Rivera <anettem.rivera@gmail.com>
2020-05-08 19:25:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,15 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  min_value = 0
-  min_item = nil
+ 
   
   hash.each do |item, quantity|
-  
+    min_value = 0
+    min_item = nil
     if min_value == 0 || quantity < min_value
       min_value = quantity 
       min_item = item
     end
   end
+  
   min_item
+
 end
