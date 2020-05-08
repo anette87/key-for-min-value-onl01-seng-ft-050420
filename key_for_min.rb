641f24dddc979aeca608a4333062ed9e3f282893
Anette Rivera <anettem.rivera@gmail.com>
2020-05-08 18:51:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(hash)
+  hash.collect.each do | object, quantity |
+    if quantity > quantity
+      return object
+    end 
+  end 
 
 end
